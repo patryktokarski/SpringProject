9d00340bf089ed27bdee608d2ba677b0e02de583
patryktokarski <patryktok@gmail.com>
2017-10-29 18:33:05 +0100

    image model

---
+++ src/main/java/com/spring/test/model/Image.java
@@ -0,0 +1,31 @@
+package com.spring.test.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "path")
+    private String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
